d7b42b4ab7d7291d17802e22fdefb97b0dd232a0
1188-25-HTL3R <1188@htl.rennweg.at>
2024-12-12 12:30:48 +0100

    Day 6: Task 2

--- src/Six.java
+++ src/Six.java
@@ -28,11 +28,140 @@ public class Six {
             map.add(line);
         }
 
+        // 1
         map = solve(map, currRow, currCol, Direction.UP);
-        //printMap(map);
         System.out.println("1: " + countChars(map, 'X'));
 
+        // 2
+        System.out.println("2: " + getAmountOfPossibleLoops(map, currRow, currCol, Direction.UP));
     }
+
+    private static class State {
+         Direction enterDirection;
+         int row;
+         int col;
+        State (Direction enterDirection, int row, int col) {
+            this.enterDirection = enterDirection;
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(row, col, enterDirection);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof State) {
+                State other = (State) obj;
+                return other.row == row && other.col == col && other.enterDirection == enterDirection;
+            }
+            return false;
+        }
+    }
+
+    private static class Point {
+        int row;
+        int col;
+        Point(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Point point)) return false;
+            return row == point.row && col == point.col;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(row, col);
+        }
+    }
+    private static int getAmountOfPossibleLoops(List<List<Character>> map, int startRow, int startCol, Direction currDir) {
+        HashMap<Point, Direction> getPossibleBlockers = new HashMap<>();
+        int[] shift = currDir.getDirectionalShift();
+        int newRow = startRow ;
+        int newCol = startCol;
+        try {
+            while (true) {
+                int totalCountOfSameBlockBounces = 0;
+                while (map.get(newRow + shift[0]).get(newCol + shift[1]) == '#') {
+                    currDir = currDir.getNextDirection();
+                    shift = currDir.getDirectionalShift();
+                    totalCountOfSameBlockBounces++;
+                    if (totalCountOfSameBlockBounces == 4) {
+                        return countLoops(map, getPossibleBlockers); // Should not happen, just as an Edge-Case
+                    }
+                }
+                newRow += shift[0];
+                newCol += shift[1];
+                Point p = new Point(newRow, newCol);
+                if (!getPossibleBlockers.containsKey(p)) {
+                    getPossibleBlockers.put(p, currDir);
+                }
+            }
+        } catch (Exception ignored) {
+            return countLoops(map, getPossibleBlockers);
+        }
+    }
+
+    private static int countLoops(List<List<Character>> map, HashMap<Point, Direction> states) {
+        int totalAmount = 0;
+        for (Map.Entry<Point, Direction> entry : states.entrySet()) {
+            if (isLoop(map, new State(entry.getValue(), entry.getKey().row, entry.getKey().col))) {
+                totalAmount++;
+            }
+        }
+        return totalAmount;
+    }
+    private static boolean isLoop(List<List<Character>> map, State startingState) {
+        int blockRow  = startingState.row;
+        int blockColumn = startingState.col;
+        Direction enterDirection = startingState.enterDirection;
+
+        char before = map.get(blockRow).get(blockColumn);
+        map.get(blockRow).set(blockColumn, '#'); // Set Blocker
+        // Go Back one, before the block
+        int[] shift = enterDirection.turnDirection().getDirectionalShift();
+        int newRow = blockRow + shift[0];
+        int newCol = blockColumn + shift[1];
+        shift = enterDirection.getDirectionalShift();
+        HashSet<State> visitedStates = new HashSet<>();
+
+        try {
+            while (true) {
+
+                int totalCountOfSameBlockBounces = 0;
+                while (map.get(newRow + shift[0]).get(newCol + shift[1]) == '#') {
+                    enterDirection = enterDirection.getNextDirection();
+                    shift = enterDirection.getDirectionalShift();
+                    totalCountOfSameBlockBounces++;
+                    if (totalCountOfSameBlockBounces == 4) {
+                        map.get(blockRow).set(blockColumn, before);
+                        return false;
+                    }
+                }
+                newRow += shift[0];
+                newCol += shift[1];
+                State currState = new State(enterDirection.copy(), newRow, newCol);
+                if (visitedStates.contains(currState)) {
+                    map.get(blockRow).set(blockColumn, before);
+                    return true; // Loop found
+                }
+                visitedStates.add(currState);
+            }
+        } catch (Exception ignored) {
+            map.get(blockRow).set(blockColumn, before);
+            return false;
+        }
+    }
+
+
+
     enum Direction {
         UP {
             @Override
@@ -44,6 +173,11 @@ public class Six {
             Direction getNextDirection() {
                 return RIGHT;
             }
+
+            @Override
+            Direction turnDirection() {
+                return DOWN;
+            }
         },
         RIGHT {
             @Override
@@ -53,6 +187,10 @@ public class Six {
             @Override
             Direction getNextDirection() {
                 return DOWN;
+            }
+            @Override
+            Direction turnDirection() {
+                return LEFT;
             }
         },
         DOWN {
@@ -64,6 +202,10 @@ public class Six {
             Direction getNextDirection() {
                 return LEFT;
             }
+            @Override
+            Direction turnDirection() {
+                return UP;
+            }
         },
         LEFT {
             @Override
@@ -74,22 +216,25 @@ public class Six {
             Direction getNextDirection() {
                 return UP;
             }
+            @Override
+            Direction turnDirection() {
+                return RIGHT;
+            }
         };
 
+        public Direction copy() {
+            return switch (this) {
+                case UP -> UP;
+                case RIGHT -> RIGHT;
+                case DOWN -> DOWN;
+                case LEFT -> LEFT;
+            };
+        }
+        abstract Direction turnDirection();
         abstract Direction getNextDirection();
         abstract int[] getDirectionalShift();
     }
-    private static int countChars(List<List<Character>> map, char c) {
-        int count = 0;
-        for (List<Character> line : map) {
-            for (char ch : line) {
-                if (ch == c) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
+
     private static List<List<Character>> solve(List<List<Character>> map, int currRow, int currCol, Direction currDir) {
         map.get(currRow).set(currCol, 'X');
         int[] shift = currDir.getDirectionalShift();
@@ -104,6 +249,19 @@ public class Six {
             return map;
         }
         return solve(map, newRow, newCol, currDir);
+    }
+
+
+    private static int countChars(List<List<Character>> map, char c) {
+        int count = 0;
+        for (List<Character> line : map) {
+            for (char ch : line) {
+                if (ch == c) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
     private static void printMap(List<List<Character>> map) {
         for (List<Character> line : map) {
